b1ccd35aa9a9e4c6b641f2217503ee2fe4d8440a
progOrbita <web.orbitadigital@gmail.com>
2023-01-17 10:18:24 +0100

    create module construct

--- od_send_email.php
+++ od_send_email.php
@@ -5,4 +5,35 @@ if (!defined('_PS_VERSION_')) {
 
 class Od_send_email extends Module
 {
+    public function __construct()
+    {
+        $this->name = 'od_send_email';
+        $this->tab = 'front_office_features'; // TODO invest
+        $this->version = '1.0.0';
+        $this->author = 'Jose Barreiro';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = [
+            'min' => '1.7.6.1',
+            'max' => '1.7.9',
+        ];
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('od_send_email');
+        $this->description = $this->l('Module to send mails.');
+
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+        $this->fields_values = [
+            '_OD_SEND_EMAIL_1_' => [
+                'translate' => $this->l('Remitente'),
+                'default' => 'web.orbitadigital@gmail.com'
+            ],
+            '_OD_SEND_EMAIL_2_' => [
+                'translate' => $this->l('Receptor'),
+                'default' => 'web.orbitadigital@gmail.com'
+            ]
+        ];
+    }
 }
