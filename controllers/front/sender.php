a7adfa549d723ccbe0b5e716b152f5da2f5af103
progOrbita <web.orbitadigital@gmail.com>
2023-01-17 11:30:57 +0100

    Update sender.php
    create postProcess functio to send mails

--- controllers/front/sender.php
+++ controllers/front/sender.php
@@ -7,4 +7,19 @@
  */
 class Od_Send_EmailSenderModuleFrontController extends ModuleFrontController
 {
+    public function postProcess()
+    {
+        if (!Tools::getIsset('nombre')) {
+            echo json_encode(['result' => false]);
+            die;
+        }
+
+        if (empty(Tools::getValue('nombre'))) {
+            echo json_encode(['result' => $this->module->mailSender($this->context->customer->id_lang, $this->module->l('Anónimo'))]);
+            die;
+        }
+
+        echo json_encode(['result' => $this->module->mailSender($this->context->customer->id_lang, Tools::getValue('nombre'))]);
+        die;
+    }
 }
