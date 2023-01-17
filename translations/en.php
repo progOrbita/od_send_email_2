a293c3d187fbfeb4c73ef4e6cfc99f501f8c618d
progOrbita <web.orbitadigital@gmail.com>
2023-01-17 11:28:08 +0100

    Create en.php
    file for english translations

---
+++ translations/en.php
@@ -0,0 +1,6 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{od_send_email}prestashop>od_send_email_54837f54143a4af6b103fb86d9f78b94'] = 'Mail send';
+$_MODULE['<{od_send_email}prestashop>sender_6be6a48ade2e9427c04259f3fc021641'] = 'anonymous';
