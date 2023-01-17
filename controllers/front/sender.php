c3c07ef249429f8601b1437c80f0ca546530340f
progOrbita <web.orbitadigital@gmail.com>
2023-01-17 11:28:39 +0100

    Create sender.php
    create frontController

---
+++ controllers/front/sender.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * <ModuleClassName> => Od_Send_Email
+ * <FileName> => sender.php
+ * Format expected: <ModuleClassName><FileName>ModuleFrontController
+ */
+class Od_Send_EmailSenderModuleFrontController extends ModuleFrontController
+{
+}
