b91724dddaf49a2b067ec4af5cf7f65ca096141a
progOrbita <web.orbitadigital@gmail.com>
2023-01-24 12:53:51 +0100

    Update ControlMails.php
    add install to class to set table on db

--- src/ControlMails.php
+++ src/ControlMails.php
@@ -2,4 +2,17 @@
 class ControlMails
 {
     private $table = "od_send_email_control";
+
+    /**
+     * install table
+     * 
+     * @return bool
+     */
+
+    public static function install(): bool
+    {
+        $string = "CREATE TABLE IF NOT EXISTS " . self::$table . " ( `id` INT NOT NULL AUTO_INCREMENT , `id_user` INT NOT NULL , `is_customer` INT(1) NOT NULL, date_send DATE, PRIMARY KEY (`id`))";
+        return Db::getInstance()->execute($string);
+    }
+
 }
