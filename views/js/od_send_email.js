e1e80579db3eb25cfdcd589c40b0432ec53baed9
progOrbita <web.orbitadigital@gmail.com>
2023-01-25 12:23:39 +0100

    Update od_send_email.js
    add params is_customer and id on ajax call

--- views/js/od_send_email.js
+++ views/js/od_send_email.js
@@ -7,7 +7,9 @@ $(document).on("click", ".od_sender", function () {
   let data = {
     nombre: window.name,
     mail: window.mail,
-    ajax: 1
+    id: window.id,
+    is_customer: window.is_customer,
+    ajax: 1,
   };
   $.ajax({
     type: "POST",
@@ -16,7 +18,9 @@ $(document).on("click", ".od_sender", function () {
     dataType: "json",
     success: function (response) {
       $(".od_result").remove();
-      $(".od_sender").after("<div class='od_result'>" + response.result + "</div>");
+      $(".od_sender").after(
+        "<div class='od_result'>" + response.result + "</div>"
+      );
     },
   });
 });
