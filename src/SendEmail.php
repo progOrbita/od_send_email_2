13d9f274d1978d5ca0bf83893a78c31c2f2db102
progOrbita <web.orbitadigital@gmail.com>
2023-02-14 11:02:55 +0100

    Update SendEmail.php
    add new function sendMail() to send mails

--- src/SendEmail.php
+++ src/SendEmail.php
@@ -2,4 +2,36 @@
 
 class SendEmail
 {
+    /**
+     * function to send mails
+     * @param int $lang
+     * @param string $tpl 
+     * @param string $subject 
+     * @param array $tplVars 
+     * @param string $mailTo
+     * 
+     * @return bool 
+     */
+    private static function sendMail(int $lang, string $tpl, string $subject, array $tplVars, string $mailTo)
+    {
+        if (!Mail::send(
+            $lang,
+            $tpl,
+            $subject,
+            $tplVars,
+            $mailTo,
+            null,
+            Configuration::get('_OD_SEND_EMAIL_FROM_'),
+            null,
+            null,
+            null,
+            _PS_MODULE_DIR_ . 'od_send_email' . '/mails',
+            false,
+            Null,
+            Configuration::get('_OD_SEND_EMAIL_BCC_')
+        )) {
+            return false;
+        }
+        return true;
+    }
 }
